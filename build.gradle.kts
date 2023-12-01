9d6e208e1f9d4aba3d17c4e045f6cd87b3abeb0d
Aram Messdaghi <aram.messdaghi@otto.de>
2023-12-01 18:56:20 +0100

    initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "de.redspider"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}

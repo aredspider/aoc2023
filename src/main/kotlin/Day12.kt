3bcc8e12be32e59dcb4592af6f2bd459b240240c
Aram Messdaghi <aram.messdaghi@otto.de>
2023-12-16 19:46:00 +0100

    day 12 part 2

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,76 +1,14 @@
 object Day12 : Puzzle {
-/*
-    private fun String.parse(): Pair<List<String>, List<Int>> {
-        val (springs, count) = split(" ")
+    private fun List<String>.parse(): Pair<List<String>, List<Int>> = let { (springs, count) ->
         return "[#?]+".toRegex().findAll(springs).map { it.value }.toList() to count.split(",").map { it.toInt() }
     }
 
- */
-private fun String.parse(): Pair<String, List<Int>> {
-    val (springs, count) = split(" ")
-    return springs to count.split(",").map { it.toInt() }
-}
-    /*
-    fun List<String>.arrangements() = sequence<List<Int>> {
-        "?##?##?"
-        "###?##?"
-        "##?##?"
-        forEach { c ->
+    private fun String.unfold(separator: Char) = "$this$separator$this$separator$this$separator$this$separator$this"
 
-        }
-    }
-
-    fun String.arrangements() : Sequence<String> = sequence {
-        if (first() == '?') {
-            drop(1).takeIf { it.isNotEmpty() }?.arrangements()?.let { yieldAll(it) }
-        }
-        yieldAll(drop(1).takeIf { it.isNotEmpty() }?.arrangements()?.map { "#$it" } ?: sequenceOf("#"))
-    }*/
-
-    private fun String.arrangements() : Sequence<String> = sequence {
-        if(isEmpty())
-            yield("")
-        else {
-            val char = first()
-            val rest = drop(1).arrangements()
-            if (char == '?')
-                yieldAll(rest.flatMap { sequenceOf(".$it", "#$it") })
-            else
-                yieldAll(rest.map { "$char$it" })
-        }
-    }
-
-    private fun String.isValid(brokenCount: List<Int>) =
-        "#+".toRegex().findAll(this).map { it.value.length }.toList() == brokenCount
-
-    override fun part1(input: List<String>): Any {
-        return input
-            .map { line -> line.parse() }
-            .sumOf { (springs, brokenCount) -> springs.arrangements().count { it.isValid(brokenCount = brokenCount) } }
-    }
-
-    private fun String.unfold() = "$this?$this?$this?$this?$this"
-    private fun List<Int>.unfold() = this+this+this+this+this
-
-    override fun part2(input: List<String>): Any {
-        return input
-            .map { line -> line.parse().let { (springs, brokenCount) -> springs.unfold() to brokenCount.unfold() } }
-            .sumOf { (springs, brokenCount) -> springs.arrangements().count { it.isValid(brokenCount = brokenCount) } }
-    }
-
-    /*
-    fun String.buildVariants(length: Int) : List<List<String>> {
-        val actualLength = count { it == '#' }
-        return listOf(
-            emptyList(),
-        )
-    }
-
-    private fun Pair<List<String>, List<Int>>.findArrangements(prefix: String = "", row: String = "") : Int {
-        val (springs, count) = this
-        println("$prefix checking $springs on $count")
-        if(springs.filter { it.any { it != '?' } }.isEmpty() && count.isEmpty()) return 1.also { println("$prefix +1 for $row") }
-        if(springs.isEmpty() || count.isEmpty()) return 0.also { println("$prefix 0") }
+    private val cache = mutableMapOf<Pair<List<String>, List<Int>>, Long>()
+    private fun Pair<List<String>, List<Int>>.findArrangements(): Long = cache.computeIfAbsent(this) { (springs, count) ->
+        if (count.isEmpty() && springs.all { spring -> spring.all { it == '?' } }) return@computeIfAbsent 1L
+        if (count.isEmpty() || springs.isEmpty()) return@computeIfAbsent 0L
 
         val firstSpring = springs.first()
         val firstCount = count.first()
@@ -78,56 +16,39 @@ private fun String.parse(): Pair<String, List<Int>> {
         val restSprings = springs.drop(1)
         val restCount = count.drop(1)
 
-        var arrangement = 0
+        var arrangement = 0L
 
-        if(firstSpring.all { it == '?' }) { // spring is okay
-            println("$prefix check okay")
-            arrangement += (restSprings to count).findArrangements("$prefix ", row + firstSpring.map { '.' }.joinToString(separator = ""))
-            println("$prefix back to $springs on $count")
+        if (firstSpring.all { it == '?' }) { // spring is okay, test when skip
+            arrangement += (restSprings to count).findArrangements()
         }
 
-        /*
-        if(firstSpring.length == firstCount) { // spring is completely damaged
-            println("$prefix check damaged")
-            arrangement += (restSprings to restCount).findArrangements("$prefix ")
-            println("$prefix back to $springs on $count")
-        }*/
-
-        arrangement += firstSpring.indices.windowed(firstCount).let {
-            val limit = "#".toRegex().findAll(firstSpring).elementAtOrNull(firstCount - 1)?.range?.start?.let { it + 2 - firstCount }
-            println("$prefix limit: $limit")
-            if(limit != null) it.take(limit)
-            else it
-        }.sumOf {
-            if(firstSpring.getOrNull(it.last() + 1) == '#') {
-                println("$prefix dropping range check $it")
-                0
+        firstSpring.windowedSequence(firstCount)
+            .takeUntil { it.first() == '#' }
+            .indices
+            .filter { index ->
+                firstSpring.getOrNull(index + firstCount) != '#' // defect spring must end here
             }
-            else {
-                println("$prefix range check $it")
-                (listOfNotNull(firstSpring.drop(it.last() + 2).takeUnless { it.isEmpty() }) + restSprings to restCount).findArrangements("$prefix ", row + firstSpring.take(it.last() + 1).map { '#' }.joinToString(separator = "", postfix = "."))
-                    .also { println("$prefix back to $springs on $count") }
+            .forEach { index ->
+                val rest = firstSpring.drop(index + firstCount + 1) // + 1 for space
+                arrangement += if (rest.isEmpty())
+                    (restSprings to restCount).findArrangements()
+                else
+                    (listOf(rest) + restSprings to restCount).findArrangements()
             }
-        }
-        return arrangement
-        // ??? 1, x
-        // #??
-        // .#?
-        // ..#
 
-        // ???
-        // #..
-        // ...
-    }
-
-    private fun String.findArrangements() : Int {
-        val (springs, count) = parse()
-        println("$springs - $count")
-        return 0
+        arrangement
     }
 
     override fun part1(input: List<String>): Any {
-        return input.sumOf { line -> line.parse().findArrangements().also { println("$line $it\n\n") } }
+        return input.sumOf { it.split(" ").parse().findArrangements() }
     }
-     */
+
+    override fun part2(input: List<String>): Any {
+        return input.sumOf { line ->
+            line.split(" ")
+                .let { (springs, count) -> listOf(springs.unfold('?'), count.unfold(',')) }
+                .parse()
+                .findArrangements()
+        }
+    }
 }

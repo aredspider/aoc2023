53333b31586d97aef4cd565ce11180ef9b4cd179
Aram Messdaghi <aram.messdaghi@otto.de>
2023-12-22 20:54:06 +0100

    day 20 part 1

---
+++ src/test/kotlin/Day20Test.kt
@@ -0,0 +1,5 @@
+class Day20Test : PuzzleTest() {
+    override val puzzle = Day20
+    override val expectedSampleResultsPart1 = listOf(32000000L, 11687500L)
+    override val expectedSampleResultsPart2 = emptyList<Nothing>()
+}

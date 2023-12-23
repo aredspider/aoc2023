4c61d389c27faf2464314d69073469a5b1f53a3a
Aram Messdaghi <aram.messdaghi@otto.de>
2023-12-23 19:43:57 +0100

    day 23 part 2

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -1,56 +1,69 @@
-import java.util.Stack
+import java.util.LinkedList
+import java.util.Queue
 
 object Day23 : Puzzle {
     override fun part1(input: List<String>): Any {
-        return input.calculateLongestPath(slippery = true)
+        return calculateLongestPath(input, slippery = true)
     }
 
     override fun part2(input: List<String>): Any {
-        return input.calculateLongestPath(slippery = false)
+        return calculateLongestPath(input, slippery = false)
     }
 
-    private fun List<String>.debugPrint(path: Iterable<Coord>) {
-        println("length: ${path.count()}")
-        forEachIndexed { y, line ->
-            line.forEachIndexed { x, c ->
-                if(Coord(x, y) in path) print('O')
-                else print(c)
+    data class Destination(val end: Coord, val distance: Int, val secondStep: Coord)
+
+    private fun calculateLongestPath(input: List<String>, slippery: Boolean): Any {
+        fun Coord.neighbours() = adjacent4
+            .filter { nextPos ->
+                val field = input.getOrNull(nextPos)
+                when {
+                    field == '#' || field == null -> false
+                    field == '.' || !slippery && (field == '<' || field == '>' || field == '^' || field == 'v') -> true
+                    field.arrowDirOrNull == nextPos - this -> true
+                    else -> false
+                }
             }
-            println()
+
+        val graph = mutableMapOf<Coord, MutableList<Destination>>()
+        val start = 1 to 0
+        val goal = input.bottomRight.left
+
+        val q : Queue<Pair<Coord, Coord>> = LinkedList()
+        q.add(start to start)
+
+        while(q.isNotEmpty()) {
+            val head = q.remove()
+            val edgeStart = head.first
+            if(graph[edgeStart]?.any { it.secondStep == head.second } == true) continue
+
+            var comingFrom = edgeStart
+            var edgeEnd = head.second
+            var neighbours: List<Coord>
+            var distance = 1
+            while(true) {
+                neighbours = edgeEnd.neighbours().filter { it != comingFrom }
+                if(neighbours.size == 1) {
+                    comingFrom = edgeEnd
+                    edgeEnd = neighbours.single()
+                    distance++
+                } else {
+                    break
+                }
+            }
+            if(neighbours.isEmpty() && edgeEnd != goal) continue
+
+            graph.getOrPut(edgeStart) { mutableListOf() }.add(Destination(end = edgeEnd, secondStep = head.second, distance = distance))
+            q.addAll(neighbours.map { nextEdgeStart -> edgeEnd to nextEdgeStart })
         }
-        println()
+
+        return (graph.calculateLongestPath(from = start, to = goal) ?: error("could not find path from $start to $goal")) - 1
     }
 
-    private fun List<String>.calculateLongestPath(slippery: Boolean): Any {
-        val q: Stack<List<Coord>> = Stack()
-        q.add(listOf(Coord(1, 0)))
-        val visited = mutableMapOf<Coord, Int>()
-        while (q.isNotEmpty()) {
-            val currentPath = q.pop()
-            val currentPos = currentPath.last()
-
-            if(currentPos == bottomRight.left) {
-                debugPrint(currentPath)
-            }
-
-            if(visited[currentPos]?.let { visitedPathLength -> visitedPathLength >= currentPath.size } == true)
-                continue
-            visited += currentPos to currentPath.size
-
-            val nextPaths = currentPos.adjacent4
-                .filter { nextPos -> nextPos !in currentPath  }
-                .filter { nextPos ->
-                    val field = getOrNull(nextPos)
-                    when {
-                        field == '.' || !slippery && (field == '<' || field == '>' || field == '^' || field == 'v') -> true
-                        field?.arrowDirOrNull == nextPos - currentPos -> true
-                        else -> false
-                    }
-                }.map { nextPos -> currentPath + nextPos }
-
-            q.addAll(nextPaths)
-        }
-
-        return visited[bottomRight.left]?.let { pathLength -> pathLength - 1 } ?: error("Could not reach exit")
+    private fun Map<Coord, List<Destination>>.calculateLongestPath(from: Coord, to: Coord, visited: Set<Coord> = setOf(from)) : Int? {
+        return getValue(from)
+            .filter { (end) -> end !in visited }
+            .mapNotNull { (end, distance) ->
+                if(end != to) calculateLongestPath(from = end, to = to, visited + end)?.plus(distance) else distance
+            }.maxOrNull()
     }
 }

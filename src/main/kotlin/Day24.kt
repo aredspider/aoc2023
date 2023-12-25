a7a30c1481cff9a5e9b19ab5c71a265da103bec1
Aram Messdaghi <aram.messdaghi@otto.de>
2023-12-25 17:57:18 +0100

    day 24 skeleton

---
+++ src/main/kotlin/Day24.kt
@@ -0,0 +1,9 @@
+object Day24 : Puzzle {
+    override fun part1(input: List<String>): Any = part1(input, 200000000000000..400000000000000)
+
+    fun part1(input: List<String>, testArea: LongRange) {
+        input.map {
+
+        }
+    }
+}
